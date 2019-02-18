2c3ebf22d35af28445de94f4537b8758fc403bb8
rummolprod999 <rummolprod999@gmail.com>
2019-02-18 20:56:39 +0300

    upd

--- Covariancy_and_Contrvariancy/Program.cs
+++ Covariancy_and_Contrvariancy/Program.cs
@@ -6,16 +6,23 @@ namespace Covariancy_and_Contrvariancy
     {
         static void Main(string[] args)
         {
+            // covariancy
             IBank<DepositAccount> depositBank = new Bank<DepositAccount>();
             Account acc1 = depositBank.CreateAccount(34);
+            DepositAccount acc_1 = depositBank.CreateAccount(34);
 
             IBank<Account> ordinaryBank = new Bank<DepositAccount>();
             Account acc2 = ordinaryBank.CreateAccount(45);
+            // DepositAccount acc_2 = ordinaryBank.CreateAccount(45);
+
+            // contrvariancy
             ITransaction<Account> accTransaction = new Transaction<Account>();
             accTransaction.DoOperation(new Account(), 400);
+            accTransaction.DoOperation(new DepositAccount(), 600);
 
-            ITransaction<DepositAccount> depAccTransaction = new Transaction<Account>();
+            ITransaction<DepositAccount> depAccTransaction = accTransaction;
             depAccTransaction.DoOperation(new DepositAccount(), 450);
+            // depAccTransaction.DoOperation(new  Account(), 500);
             Console.Read();
         }
     }
