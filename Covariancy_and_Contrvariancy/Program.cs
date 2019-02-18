29fa92f349332220509be930d12a0e20698a724e
rummolprod999 <rummolprod999@gmail.com>
2019-02-18 19:41:05 +0300

    Добавьте файлы проекта.

---
+++ Covariancy_and_Contrvariancy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Covariancy_and_Contrvariancy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
